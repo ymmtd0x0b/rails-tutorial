3373aa88c05f65269a6eeb0cac2de55a24d7177c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-12-18 19:23:59 +0900

    Userモデルのemail属性の一意性を検証するテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,4 +46,11 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address} should be invalid"
     end
   end
+
+  test 'email adresses should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
