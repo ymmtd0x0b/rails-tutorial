d7b048a8eb96f0e26704ff12bb5cc9464393aabd
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-12-22 01:25:13 +0900

    ユーザー登録に成功した場合の処理を追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,4 +18,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'li', text: 'Password is too short (minimum is 6 characters)'
     end
   end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'foobar',
+                                         password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
