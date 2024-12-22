855408cbfd1164b7e5742e162ce27f0c807cd221
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-12-23 01:35:25 +0900

    8.2.4 演習 1,2 解答

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,12 +5,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test 'login with invalid information' do
+  test 'login with valid email/invalid password' do
     get login_path
     assert_template 'sessions/new'
 
-    post login_path, params: { session: { email: '',
-                                          password: '' } }
+    post login_path, params: { session: { email: 'michael@example.com',
+                                          password: 'invalid' } }
     assert_template 'sessions/new'
     assert flash.any?
 
