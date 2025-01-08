4d72892ca9b70c92f2c19196831c551843969bf1
ymmtd0x0b <ymmtd0x0b@gmail.com>
2025-01-09 03:14:43 +0900

    マイクロポストの投稿画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,14 +54,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_path
-    end
-  end
-
   def correct_user
     user = User.find(params[:id])
     unless current_user?(user)
