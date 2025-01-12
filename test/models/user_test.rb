7f0c100a5c998366d37c9db1bbc244b4441fa469
ymmtd0x0b <ymmtd0x0b@gmail.com>
2025-01-13 04:48:48 +0900

    完全なフィードの実装

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -96,4 +96,25 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+  test 'feed should have the right posts' do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    # フォローしているユーザーのポストが含まれている
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # 自分自身のポストが含まれている
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # フォローしていないユーザーのポストは含まれない
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
